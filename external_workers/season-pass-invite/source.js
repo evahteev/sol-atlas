c9ab6da4ecc16afd73d176b5adcb5441bee220ae
Evgeny Shestakov <evgeny.shestakov@techusage.com>
2024-05-22 13:03:01 +0200

    generate merkle tree with openzeppelin lib

--- external_workers/season-pass-invite/source.js
+++ external_workers/season-pass-invite/source.js
@@ -1,16 +1,45 @@
-const {MerkleTree} = await import('npm:merkletreejs');
-const {keccakFromHexString, keccak256} = await import('https://deno.land/x/npm_ethereumjs_util@0.0.3/mod.ts');
-
+// const {StandardMerkleTree} = await import('npm:@openzeppelin/merkle-tree');
+import {StandardMerkleTree} from '@openzeppelin/merkle-tree';
 
 function getExistingAddresses() {
-    const existingAddresses = Functions.HTTPGet("https://clfunctions.s3.eu-north-1.amazonaws.com/offchain-secrets.json")
+    // return dict {tokenId: [address1, address2, ...]}
+    return Functions.makeHttpRequest(
+        {
+            url: 'https://gurunetwork.ai/api/invited_wallets',
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-SYS-KEY': ''
+            }
+        }
+    )
 }
 
-function generateMerkletree(addresses) {
-    const set = Array.from(new Set(addresses));
-    const leafNodes = set.map((address) => keccakFromHexString(address));
-    return new MerkleTree(leafNodes, keccak256, {sortPairs: true});
+
+function generateMerkletree(tokenIdsToAddressesString) {
+    const tokenIdsToAddressesDict = JSON.parse(tokenIdsToAddressesString);
+    const tokenIdToAddress = []
+    for (const tokenId in tokenIdsToAddressesDict) {
+        for (const address of tokenIdsToAddressesDict[tokenId]) {
+            tokenIdToAddress.push([tokenId, address]);
+        }
+    }
+    console.log(tokenIdToAddress)
+    return StandardMerkleTree.of(tokenIdToAddress, ['uint', 'address']);
 }
 
-const tree = generateMerkletree(existingAddresses.concat(args))
-return tree.getRoot();
+const existingWallets = getExistingAddresses();
+
+// const tree = generateMerkletree(`{
+//     "1": ["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"],
+//     "2": ["0x3da87b1c3743BD2dA60DF2ef1BC6F26Ef9Da6086"]
+// }`);
+const tree = generateMerkletree(existingWallets);
+
+for (const [i, v] of tree.entries()) {
+    if (v[1] === '0x3da87b1c3743BD2dA60DF2ef1BC6F26Ef9Da6086') {
+        const proof = tree.getProof(i);
+        console.log('Value:', v);
+        console.log('Proof:', proof);
+    }
+}
