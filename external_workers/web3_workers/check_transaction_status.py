6859d3a95f0af0aef3697328184ebe5ab08f3a74
Evgeny Shestakov <evgeny.shestakov@techusage.com>
2024-05-29 19:45:33 +0200

    improve update merkle root

--- external_workers/web3_workers/check_transaction_status.py
+++ external_workers/web3_workers/check_transaction_status.py
@@ -3,11 +3,11 @@ from camunda.external_task.external_task_worker import ExternalTaskWorker
 from web3 import Web3
 import os
 
-TOPIC_NAME = os.getenv('TOPIC_NAME', "CheckTransactionConfirmed")
-WEB3_URL = os.getenv('WEB3_URL', 'http://new-rpc-gw-prod.dexguru.biz/archive/261')
-CAMUNDA_URL = os.getenv('CAMUNDA_URL', 'http://localhost:8080/engine-rest')
-CAMUNDA_USERNAME = os.getenv('CAMUNDA_USERNAME', 'demo')
-CAMUNDA_PASSWORD = os.getenv('CAMUNDA_PASSWORD', 'demo')
+TOPIC_NAME = os.getenv("TOPIC_NAME", "CheckTransactionConfirmed")
+WEB3_URL = os.getenv("WEB3_URL", "http://new-rpc-gw-prod.dexguru.biz/archive/261")
+CAMUNDA_URL = os.getenv("CAMUNDA_URL", "http://localhost:8080/engine-rest")
+CAMUNDA_USERNAME = os.getenv("CAMUNDA_USERNAME", "demo")
+CAMUNDA_PASSWORD = os.getenv("CAMUNDA_PASSWORD", "demo")
 
 w3 = Web3(Web3.HTTPProvider(WEB3_URL))
 
@@ -19,7 +19,7 @@ default_config = {
     "asyncResponseTimeout": 5000,
     "retries": 3,
     "retryTimeout": 15000,
-    "sleepSeconds": 30
+    "sleepSeconds": 30,
 }
 
 
@@ -29,7 +29,7 @@ def check_transaction_status(tx_hash: str) -> bool:
     if receipt is None:
         return False
 
-    if receipt['status'] != 1:
+    if receipt["status"] != 1:
         return False
     return True
 
@@ -40,10 +40,10 @@ def check_transaction_data(tx_hash: str, value: int, txn_input: bytes) -> bool:
         if not transaction:
             return False
 
-        if transaction['value'] != int(value):
+        if transaction["value"] != int(value):
             return False
 
-        if transaction['input'] != txn_input:
+        if transaction["input"] != txn_input:
             return False
 
         return True
@@ -62,25 +62,39 @@ def handle_task(task: ExternalTask) -> TaskResult:
         txn_input = bytes.fromhex(txn_input[2:])
 
     if not tx_hash:
-        return task.failure("Transaction hash not provided", "Transaction hash is missing from the variables", 3, 15000)
+        return task.failure(
+            "Transaction hash not provided",
+            "Transaction hash is missing from the variables",
+            3,
+            15000,
+        )
 
     is_transaction_same = check_transaction_data(tx_hash, txn_value, txn_input)
 
     if not is_transaction_same:
-        return task.failure("Transaction not confirmed", "The transaction has not been confirmed on the blockchain", 3,
-                            15000)
+        return task.failure(
+            "Transaction not confirmed",
+            "The transaction has not been confirmed on the blockchain",
+            3,
+            15000,
+        )
 
     transaction_success = check_transaction_status(tx_hash)
     if not transaction_success:
         return task.bpmn_error(
             "transaction_failed_onchain",
             "Transaction receipt status is not success in blockchain",
-            variables
+            variables,
         )
     return task.complete(variables)
 
 
-if __name__ == '__main__':
-    ExternalTaskWorker(worker_id="1",
-                       base_url=CAMUNDA_URL,
-                       config=default_config).subscribe([TOPIC_NAME], handle_task)
+if __name__ == "__main__":
+    ExternalTaskWorker(
+        worker_id="1", base_url=CAMUNDA_URL, config=default_config
+    ).subscribe(
+        [
+            TOPIC_NAME,
+        ],
+        handle_task,
+    )
