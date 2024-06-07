22c19714fd32132e7e2f7ecfa20ec7b757ef348c
kelar86 <volynshchikov@gmail.com>
2024-06-08 05:08:33 +0700

    chore rabbitMQ serialization (#20)
    Co-authored-by: Alexey <alex.volynshchikov@techusage.com>

---
+++ engine/src/main/java/ai/hhrdr/chainflow/engine/config/RabbitMQConfig.java
@@ -0,0 +1,24 @@
+package ai.hhrdr.chainflow.engine.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate customRabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
