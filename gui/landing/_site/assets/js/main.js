a703057e70bc30c9807ce2b78a985d9f0482223c
Diana-Kovalyova <dianaborysivna@gmail.com>
2024-06-13 21:55:47 +0300

    chore: add button

--- gui/landing/_site/assets/js/main.js
+++ gui/landing/_site/assets/js/main.js
@@ -22,4 +22,42 @@ const menuToggler = () => {
 
 window.addEventListener("load", () => {
   menuToggler();
+
+  const addTokenToMetmaskId = document.getElementById('addTokenToMetmaskId')
+  if(addTokenToMetmaskId){
+    addTokenToMetmaskId.onclick = addTokenToMetmask;
+  }
 });
+
+
+const addTokenToMetmask = async () => {
+  const options = {
+    address: "0x525574C899A7c877a11865339e57376092168258",
+    symbol: "GURU",
+    decimals: 18,
+    image: "https://assets.coingecko.com/coins/images/38583/standard/tGURU_token_circle.png"
+  }
+  
+  try {
+    const wasAdded = await window.ethereum
+      .request({
+        method: "wallet_watchAsset",
+        params: {
+          type: "ERC20",
+          options
+        },
+      });
+  
+    if (wasAdded) {
+      console.log("Thanks for your interest!");
+    } else {
+      console.log("Your loss!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+
+
